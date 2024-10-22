9eeb41bfe35ebe88f70826312f2ac364f95e8b5d
LuisCamachoRivero <camachoriveroluis@gmail.com>
2024-10-22 00:04:10 +0000

    Prueba Streamlit 0.6

--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@
 from pickle import load
 import streamlit as st
 
-model = load(open('../models/decission_tree_classifier_42_pca.sav','rb'))
-pca = load(open('../models/pca_4.sav','rb'))
+model = load(open('/workspaces/Streamlit/models/decission_tree_classifier_42_pca.sav','rb'))
+pca = load(open('/workspaces/Streamlit/models/pcs_4.sav','rb'))
 
 st.title('Prediction Exam Score')
 
@@ -22,4 +22,59 @@ horas_sueno = st.slider('Sleep Hours', min_value=4, max_value=10, step=1)
 nota_previa = st.slider('Previous Score', min_value=50, max_value=100, step=5)
 tutoria = st.slider('Tutoring Sessions', min_value=0, max_value=9, step=1)
 act_fisica = st.slider('Physical Activity (hours)', min_value=0, max_value=6, step=1)
-calidad_profe = 
+act_extra = st.toggle('Extracurricular Activities')
+if act_extra:
+    st.write("Feature activated!")
+involucramiento_parental = st.radio(
+    "Parental Involvement",
+    ["Low", "Mid", "High"],
+    index=None
+)
+tipo_escuela = st.radio(
+    "School Type",
+    ["Public", "Private"],
+    index=None
+)
+nivel_educ_padres = st.radio(
+    "Parental Education Level",
+    ["Mid", "Bachelor", "University", "None"],
+    index=None
+)
+influencia = st.radio(
+    "Peer Influence",
+    ["Positive", "Negative", "Neutral"],
+    index=None
+)
+Family_income_num=0
+Access_to_Resources_num=0
+Motivation_Level_num=0
+Distance_from_Home_num=0
+Teacher_Quality_num=0
+Internet_Access_num=0
+Learning_Disabilities_num=0
+
+env_parental_dict = {'Low':0,
+                     'Mid':1,
+                     'High':2
+
+}
+
+tipo_escuela_dict = {'Public':0,
+                     'Private':1
+
+}
+
+nivel_educ_padres_dic= {'Mid':0,
+                        'Bachelor':1,
+                        'University':2,
+                        'None':-1
+}
+
+influencia_dic = {'Positive':0,
+                  'Negative':1,
+                  'Neutral':2
+
+}
+
+if st.button('Prediction'):
+    conversion_pca = horas_estudio,asistencia,horas_sueno,nota_previa,tutoria
