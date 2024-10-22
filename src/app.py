78c776db1f2ab414b98a9750faaf4d0769b457e0
LuisCamachoRivero <camachoriveroluis@gmail.com>
2024-10-22 00:16:00 +0000

    Prueba Streamlit 0.7

--- src/app.py
+++ src/app.py
@@ -7,15 +7,6 @@ pca = load(open('/workspaces/Streamlit/models/pcs_4.sav','rb'))
 
 st.title('Prediction Exam Score')
 
-#'Hours_Studied', 'Attendance', 'Sleep_Hours', 'Previous_Scores',
-#'Tutoring_Sessions', 'Physical_Activity', 'Teacher_Quality_num',
-#'Gender_num', 'Extracurricular_Activities_num',
-#'Parental_Education_Level_num', 'Parental_Involvement_num',
-#'Learning_Disabilities_num', 'Peer_Influence_num',
-#'School_Type_num', 'Family_Income_num', 'Distance_from_Home_num',
-#'Access_to_Resources_num', 'Motivation_Level_num',
-#'Internet_Access_num'
-
 horas_estudio = st.slider('Hours Studied', min_value=0, max_value=44, step=1)
 asistencia = st.slider('Attendance', min_value=60, max_value=100, step=1)
 horas_sueno = st.slider('Sleep Hours', min_value=4, max_value=10, step=1)
@@ -24,7 +15,7 @@ tutoria = st.slider('Tutoring Sessions', min_value=0, max_value=9, step=1)
 act_fisica = st.slider('Physical Activity (hours)', min_value=0, max_value=6, step=1)
 act_extra = st.toggle('Extracurricular Activities')
 if act_extra:
-    st.write("Feature activated!")
+    st.write("Yes")
 involucramiento_parental = st.radio(
     "Parental Involvement",
     ["Low", "Mid", "High"],
@@ -55,26 +46,19 @@ Learning_Disabilities_num=0
 
 env_parental_dict = {'Low':0,
                      'Mid':1,
-                     'High':2
-
-}
+                     'High':2}
 
 tipo_escuela_dict = {'Public':0,
-                     'Private':1
-
-}
+                     'Private':1}
 
 nivel_educ_padres_dic= {'Mid':0,
                         'Bachelor':1,
                         'University':2,
-                        'None':-1
-}
+                        'None':-1}
 
 influencia_dic = {'Positive':0,
                   'Negative':1,
-                  'Neutral':2
-
-}
+                  'Neutral':2}
 
 if st.button('Prediction'):
-    conversion_pca = horas_estudio,asistencia,horas_sueno,nota_previa,tutoria
+    conversion_pca = pca.transform([horas_estudio,asistencia,horas_sueno,nota_previa,tutoria,act_fisica,])
